7bb526ee56c6d79686dae71814d8afe79a7d3b66
Aaron Newton <aanewton@deloitte.com.au>
2021-06-10 11:05:20 +1000

    Added review generation logic to the review generation trigger.

--- src/ReviewsWorkerFunctionApp/ReviewGenerator.cs
+++ src/ReviewsWorkerFunctionApp/ReviewGenerator.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,6 +6,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Bogus;
 
 namespace ReviewsWorkerFunctionApp
 {
@@ -17,19 +17,23 @@ namespace ReviewsWorkerFunctionApp
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"Generating review with {nameof(ReviewGenerator)}.");
 
-            string name = req.Query["name"];
+            var faker = new Faker("en");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var messageString = JsonConvert.SerializeObject(
+                new
+                {
+                    EventId = Guid.NewGuid(),
+                    EventType = "ReviewSubmitted",
+                    Content = new {
+                        Text = faker.Rant.Review()
+                    }
+                });
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            log.LogInformation($"Sending review: {messageString}");
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(messageString);
         }
     }
 }
